1c554c023a2b4380cb048cece324681141056e85
saban <sabanamdari20@gmail.com>
2024-09-16 15:47:04 -0400

    creating the sign up form and sign up page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage.jsx';
 import "./App.scss";
 import AboutUsPage from './pages/AboutUsPage/AboutUsPage.jsx';
+import SignInPage from './pages/SignInPage/SignInPage.jsx';
+import SignUpPage from './pages/SignUpPage/SignUpPage.jsx';
+
 
 const App = () => {
  
@@ -10,6 +13,8 @@ const App = () => {
 			<Routes>
 				<Route path='/' element={<HomePage />}></Route>
 				<Route path='/about-us' element={<AboutUsPage />}></Route>
+				<Route path='/sign-in' element={<SignInPage />}></Route>
+				<Route path='/sign-up' element={<SignUpPage />}></Route>
 			</Routes>
 		</BrowserRouter>
 
---
+++ client/src/pages/SignInPage/SignInPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import SignUpForm from "../../component/SignUpForm/SignUpForm.jsx";
+import Header from "../../component/Header/Header.jsx";
+import Footer from "../../component/Footer/Footer.jsx";
+import signup from "../../assets/images/signup.jpg";
+import "./SignInPage.scss";
+
+const SignInPage = () => {
+  return (
+    <div className="signup">
+      <Header />
+    
+      <Footer />
+    </div>
+  );
+};
+
+export default SignInPage;
